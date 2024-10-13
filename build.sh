d1a3a7d9d4e741fe6d638c99174089e3733dd45b
Bohdan Mart <mart.bogdan@gmail.com>
2024-10-13 22:27:35 +0300

    Changed `build.sh` to be able to help troubleshoot
    It would fail script on any error.
    It would print each executed command.

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+# fail whole script if any command fail
+set -e
+
+shopt -s expand_aliases
+alias trace_on='set -x'
+alias trace_off='{ PREV_STATUS=$? ; set +x; } 2>/dev/null; (exit $PREV_STATUS)'
+
+
+# echho commands being executed
+trace_on
+
 mkdir -p build
 
 nasm -f elf64 -gdwarf -o build/util.o src/util.asm
@@ -6,3 +18,7 @@ nasm -f elf64 -gdwarf -o build/levels.o src/levels.asm
 nasm -f elf64 -gdwarf -o build/main.o src/main.asm
 
 ld -o build/gems build/util.o build/draw.o build/main.o build/levels.o
+
+trace_off
+
+echo "Done!"
